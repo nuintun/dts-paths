284d35147fe0a8e1ccc1c19c0ca98c97be33f334
nuintun <nuintun@qq.com>
2024-12-12 11:32:05 +0800

    feat: Update code

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,38 @@
  * @module index
  */
 
-import { Project } from 'ts-morph';
 import { resolve } from 'node:path';
+import { Project, ts } from 'ts-morph';
 
 const project = new Project({
-  tsConfigFilePath: resolve('tests/tsconfig.json')
+  skipAddingFilesFromTsConfig: true,
+  tsConfigFilePath: resolve('tests/tsconfig.json'),
+  resolutionHost(moduleResolutionHost, getCompilerOptions) {
+    return {
+      resolveModuleNames(moduleNames, containingFile) {
+        const compilerOptions = getCompilerOptions();
+        const resolvedModules: (ts.ResolvedModule | undefined)[] = [];
+
+        for (const moduleName of moduleNames) {
+          const { resolvedModule } = ts.resolveModuleName(
+            moduleName,
+            containingFile,
+            compilerOptions,
+            moduleResolutionHost,
+            ts.createModuleResolutionCache('.ts', name => name)
+          );
+
+          resolvedModules.push(resolvedModule);
+
+          if (resolvedModule && !resolvedModule.isExternalLibraryImport) {
+            console.log(moduleName, resolvedModule.resolvedFileName);
+          }
+        }
+
+        return resolvedModules;
+      }
+    };
+  }
 });
 
 const sourceFiles = project.addSourceFilesAtPaths('tests/types/**/*.{ts,cts}');
