bd378f44b0307317290dc9824e5f57101bb3e759
nuintun <nuintun@gmail.com>
2026-03-26 15:29:17 +0800

    test: use static snapshots for resolvePaths output

---
+++ tests/snapshots/encoder/utils/matrix.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @module matrix
+ */
+import { ECLevel } from '../../common/ECLevel.js';
+import { BitArray } from '../../common/BitArray.js';
+import { ByteMatrix } from '../../common/ByteMatrix.js';
+import { Version } from '../../common/Version.js';
+export declare function buildMatrix(
+  codewords: BitArray,
+  version: Version,
+  ecLevel: ECLevel,
+  mask: number
+): ByteMatrix;
